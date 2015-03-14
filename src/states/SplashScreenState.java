e3757f997b5f331371344a226d370dd966e59444
Daniel Braikeh <danbraik@hotmail.com>
2015-03-14 23:48:06 +0100

    Add Logos

--- src/states/SplashScreenState.java
+++ src/states/SplashScreenState.java
@@ -40,13 +40,12 @@ public class SplashScreenState extends AbstractApplicationState {
         
         current = sfml;
         mClock = new Clock();
-        state = 1;
+        state = 1;     
     }
 
 
     @Override
     public void handleEvent(Event event) {
-        
         if (event.type == Event.Type.KEY_PRESSED) {
             getAppContent().goToState(Main.MyStates.MAINMENUSTATE);
         }
@@ -54,29 +53,28 @@ public class SplashScreenState extends AbstractApplicationState {
 
     @Override
     public void update(Time time) {
-        if (state == 1 && mClock.getElapsedTime().asSeconds() > 2) {
+        if (state == 1 && mClock.getElapsedTime().asSeconds() > 1) {
             state = 2;
             mClock.restart();
             current = casemate;
-        } else if (state == 2 && mClock.getElapsedTime().asSeconds() > 2) {
+        } else if (state == 2 && mClock.getElapsedTime().asSeconds() > 1) {
             state = 3;
             mClock.restart();
             current = game;
-        } else if (state == 3 && mClock.getElapsedTime().asSeconds() > 2) {
+        } else if (state == 3 && mClock.getElapsedTime().asSeconds() > 1) {
             getAppContent().goToState(Main.MyStates.MAINMENUSTATE);
         }
     }
 
     @Override
     public void render() {
-        // TODO
+        
         final RenderTarget target = getGraphicEngine().getRenderTarget();
         target.clear(Color.BLACK);
 
-        FloatRect a = current.getLocalBounds();
-//        float 
-//
-//        current.setOrigin( a. );
+        FloatRect a = current.getGlobalBounds();
+        
+        current.setOrigin(a.width / 2, a.height / 2);
         target.draw(current);
     }
 
